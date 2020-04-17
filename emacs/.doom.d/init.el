8a1f60e26fcd3a4a1fd637454ada9851b00fa33b
Pedro Alves <pta2002@pta2002.com>
2020-04-17 17:24:35 +0100

    Configure emacs

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -29,7 +29,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;fill-column       ; a `fill-column' indicator
+       ;; fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
@@ -39,7 +39,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;pretty-code       ; replace bits of code with pretty symbols
-       ;;tabs              ; an tab bar for Emacs
+       tabs              ; an tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
@@ -176,5 +176,3 @@
        :config
        ;;literate
        (default +bindings +smartparens))
-
-;; (projectile-add-known-project "~/Uni/2sem/li2")
