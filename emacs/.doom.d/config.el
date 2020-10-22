9a582b8fac744b2f907a569a7703706c37584cf4
Pedro Alves <pta2002@pta2002.com>
2020-10-22 09:25:43 +0100

    Increase emacs font size

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -26,7 +26,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox
-      doom-font (font-spec :family "JetBrains Mono" :size 14)
+      doom-font (font-spec :family "JetBrains Mono" :size 15)
       doom-big-font (font-spec :family "JetBrains Mono" :size 20))
 
 ;; If you use `org' and don't want your org files in the default location below,
